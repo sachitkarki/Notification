efb0f1c0546c900335319e3655af392eb501d5a9
yangjinpeng <yangjinpeng@easymob.com.cn>
2015-11-23 17:37:31 +0800

    create a base notificaion

--- src/main/java/cn/dev/MainActivity.java
+++ src/main/java/cn/dev/MainActivity.java
@@ -1,21 +1,31 @@
 package cn.dev;
 
 import android.app.Activity;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.ViewGroup;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 
 public class MainActivity extends Activity {
+
+    public int ID = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        baseNotification();
     }
 
-    public void gitTest1(){
-        ViewGroup v = null;
+    public void baseNotification(){
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        builder.setContentTitle("title");
+        builder.setContentText("content");
+        builder.setAutoCancel(true);
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher));
+
+        NotificationManagerCompat.from(this).notify(ID,builder.build());
     }
 }
